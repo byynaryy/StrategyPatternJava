600803c78e1538f05fe531201d74aa6c7386bf87
Jeff <jeff@Jeffs-MacBook-Pro.local>
2024-02-03 16:16:30 +0100

    refactor(Duck): change Duck class to abstract

--- src/main/java/org/awesome/Duck.java
+++ src/main/java/org/awesome/Duck.java
@@ -1,16 +1,18 @@
 package org.awesome;
 
-public class Duck {
+public abstract class Duck {
     FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
+
+    public Duck() {}
     public void doFly() {
         flyBehavior.fly();
     }
     public void doQuack() {
         quackBehavior.quack();
     }
-    //swim();
-    public void display() {
-        System.out.println("Duck");
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
     }
+    public abstract void display();
 }
